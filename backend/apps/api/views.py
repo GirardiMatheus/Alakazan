874164fc8fc5f42c4e03694b5287c21ff1c225d1
Matheus Girardi <girardimatheus27@gmail.com>
2025-03-12 10:09:47 -0300

    fix: use Django ORM in DataView to access IngestionData model

--- backend/apps/api/views.py
+++ backend/apps/api/views.py
@@ -1,29 +1,8 @@
 from django.http import JsonResponse
 from django.views import View
-from apps.ingestion.models import IngestionData
-import psycopg2
-import os
+from apps.ingestion.models import IngestionData  
 
 class DataView(View):
     def get(self, request):
-        DB_NAME = os.getenv("POSTGRES_DB", "alakazan_db")
-        DB_USER = os.getenv("POSTGRES_USER", "postgres")
-        DB_PASS = os.getenv("POSTGRES_PASSWORD", "postgres")
-        DB_HOST = "db"
-        DB_PORT = "5432"
-
-        conn = psycopg2.connect(
-            dbname=DB_NAME,
-            user=DB_USER,
-            password=DB_PASS,
-            host=DB_HOST,
-            port=DB_PORT
-        )
-        cursor = conn.cursor()
-
-        cursor.execute("SELECT * FROM ingestion_data")
-        rows = cursor.fetchall()
-
         data = IngestionData.objects.all().values('id', 'message')
-
         return JsonResponse(list(data), safe=False)
